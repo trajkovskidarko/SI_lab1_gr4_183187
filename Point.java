0b6a02a8266dc8261a90eb8f089df2ed45e14c9e
trajkovskidare <darko.trajkovski99@yahoo.com>
2020-04-12 18:02:17 +0200

    point class finished

--- Point.java
+++ Point.java
@@ -2,19 +2,55 @@ class Point {
 	String id
 	double x,y;
 	String boja;
+	
+	public Point(String id, double x, double y, String boja){
+		this.id = id;
+		this.x = x;
+		this.y = y;
+		this.boja = boja;
+	}
+	
+	public void setID(String newID){
+		this.id = newID;
+	}
+	
+	public void setX(double newX){
+		this.x = newX;
+	}
+	
+	public void setY(double newY){
+		this.y = newY;
+	}
+	
+	public void setBoja(String newBoja){
+		this.boja = newBoja;
+	}
+	
+	public String getID(){
+		return this.id;
+	}
 
-	//TODO add new variable
-
-	//TODO constructor
-
-	//TODO setters and getters
-
+	public double getX(){
+		return this.x;
+	}
+	
+	public double getY(){
+		return this.y;
+	}
+	
+	public String getBoja(){
+		return this.boja;
+	}
+	
 	public void move (char xDirection, char yDirection) {
-		//TODO
+		if(xDirection == 'L') x -= 1;
+		else if(xDirection == 'R') x += 1;
+		if(yDirection == 'U') y += 1;
+		else if(yDirection == 'D') y -= 1;
 	}
 
 	public void draw () {
-		//TODO
+		System.out.println("[" + x + "," + y + "]");
 	}
 
 
